6308004357d38ed3b7825413a3570437b066b9b6
MarceloFullstack <fotofacil20@gmail.com>
2023-03-03 19:52:12 -0300

    feat: organizacao de modulos

---
+++ dist/cache-with-duration-indexed-db.js
@@ -0,0 +1,66 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.clearCacheIndexedDB = exports.cacheWithDurationIndexedDB = void 0;
+const cacheWithDurationIndexedDB = async (cacheKey, cacheTime, fn, log = false) => {
+    const dbName = 'cacheDB';
+    const storeName = 'cacheStore';
+    let db = null;
+    const openDB = () => {
+        return new Promise((resolve, reject) => {
+            const request = window.indexedDB.open(dbName, 1);
+            request.onerror = (event) => reject(event.target.error);
+            request.onsuccess = (event) => resolve(event.target.result);
+            request.onupgradeneeded = (event) => {
+                const db = event.target.result;
+                db.createObjectStore(storeName, { keyPath: 'key' });
+            };
+        });
+    };
+    const getData = async (key) => {
+        return new Promise((resolve, reject) => {
+            const transaction = db.transaction([storeName], 'readonly');
+            const store = transaction.objectStore(storeName);
+            const request = store.get(key);
+            request.onerror = (event) => reject(event.target.error);
+            request.onsuccess = (event) => resolve(event.target.result);
+        });
+    };
+    const setData = async (key, value) => {
+        return new Promise((resolve, reject) => {
+            const transaction = db.transaction([storeName], 'readwrite');
+            const store = transaction.objectStore(storeName);
+            const request = store.put(Object.assign({ key }, value));
+            request.onerror = (event) => reject(event.target.error);
+            request.onsuccess = (event) => resolve();
+        });
+    };
+    if (!db) {
+        db = await openDB();
+    }
+    let now = Date.now();
+    let cachedData = await getData(cacheKey);
+    if (cachedData && now - cachedData.timestamp < cacheTime) {
+        log && console.info('returned from cache', cacheKey);
+        return cachedData.value;
+    }
+    log && console.info('returned from API', cacheKey);
+    let data = await fn();
+    await setData(cacheKey, { value: data, timestamp: now });
+    return data;
+};
+exports.cacheWithDurationIndexedDB = cacheWithDurationIndexedDB;
+const clearCacheIndexedDB = () => {
+    return new Promise((resolve, reject) => {
+        const request = window.indexedDB.open('cacheDB', 1);
+        request.onerror = (event) => reject(event.target.error);
+        request.onsuccess = (event) => {
+            const db = event.target.result;
+            const transaction = db.transaction(['cacheStore'], 'readwrite');
+            const store = transaction.objectStore('cacheStore');
+            const request = store.clear();
+            request.onerror = (event) => reject(event.target.error);
+            request.onsuccess = () => resolve();
+        };
+    });
+};
+exports.clearCacheIndexedDB = clearCacheIndexedDB;
--- dist/index.js
+++ dist/index.js
@@ -1,67 +1,17 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.clearCacheIndexedDB = exports.cacheWithDuration = void 0;
-const cacheWithDuration = async (cacheKey, cacheTime, fn, log = false) => {
-    console.log('teste');
-    const dbName = 'cacheDB';
-    const storeName = 'cacheStore';
-    let db = null;
-    const openDB = () => {
-        return new Promise((resolve, reject) => {
-            const request = window.indexedDB.open(dbName, 1);
-            request.onerror = (event) => reject(event.target.error);
-            request.onsuccess = (event) => resolve(event.target.result);
-            request.onupgradeneeded = (event) => {
-                const db = event.target.result;
-                db.createObjectStore(storeName, { keyPath: 'key' });
-            };
-        });
-    };
-    const getData = async (key) => {
-        return new Promise((resolve, reject) => {
-            const transaction = db.transaction([storeName], 'readonly');
-            const store = transaction.objectStore(storeName);
-            const request = store.get(key);
-            request.onerror = (event) => reject(event.target.error);
-            request.onsuccess = (event) => resolve(event.target.result);
-        });
-    };
-    const setData = async (key, value) => {
-        return new Promise((resolve, reject) => {
-            const transaction = db.transaction([storeName], 'readwrite');
-            const store = transaction.objectStore(storeName);
-            const request = store.put(Object.assign({ key }, value));
-            request.onerror = (event) => reject(event.target.error);
-            request.onsuccess = (event) => resolve();
-        });
-    };
-    if (!db) {
-        db = await openDB();
-    }
-    let now = Date.now();
-    let cachedData = await getData(cacheKey);
-    if (cachedData && now - cachedData.timestamp < cacheTime) {
-        log && console.info('returned from cache', cacheKey);
-        return cachedData.value;
-    }
-    log && console.info('returned from API', cacheKey);
-    let data = await fn();
-    await setData(cacheKey, { value: data, timestamp: now });
-    return data;
-};
-exports.cacheWithDuration = cacheWithDuration;
-const clearCacheIndexedDB = () => {
-    return new Promise((resolve, reject) => {
-        const request = window.indexedDB.open('cacheDB', 1);
-        request.onerror = (event) => reject(event.target.error);
-        request.onsuccess = (event) => {
-            const db = event.target.result;
-            const transaction = db.transaction(['cacheStore'], 'readwrite');
-            const store = transaction.objectStore('cacheStore');
-            const request = store.clear();
-            request.onerror = (event) => reject(event.target.error);
-            request.onsuccess = () => resolve();
-        };
-    });
-};
-exports.clearCacheIndexedDB = clearCacheIndexedDB;
+__exportStar(require("./cache-with-duration-indexed-db"), exports);
